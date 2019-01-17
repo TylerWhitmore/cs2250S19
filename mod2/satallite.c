7fa2f9987b63938ed1c3982ae0ce62a5d8077d7d
Tyler Whitmore <tylerwhitmore@mail.weber.edu>
2019-01-17 10:18:43 -0700

    Added satallite

---
+++ mod2/satallite.c
@@ -0,0 +1,46 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  satallite.c
+ *
+ *    Description:  calculate altitude of satallite
+ *
+ *        Version:  1.0
+ *        Created:  01/17/2019 09:47:27 AM
+ *       Revision:  none
+ *       Compiler:  gcc satallite.c -o satallite.out -lm
+ *
+ *         Author:  Tyler Whitmore (), tylerwhitmore@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <math.h>
+// Constants
+const double GRAVITY = 6.67e-11; //Newton's Constant
+const double MASSEARTH = 5.97e24; //kg
+const double RADIUSEARTH = 6.371e6; //Radius of Earth in meters
+const double PI = 3.1415941;
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    double period = 0.0; // in seconds
+    double height = 0.0; // in meters
+    //1) Enter the period
+    printf("Enter the period in seconds: \n");
+    scanf("%lf",&period);
+    //formula: height=((Gravity*MassOfEarth*Time^2)/4pi^2)^1/3 -RadiusOfEarth
+    double top = GRAVITY*MASSEARTH*pow(period,2);
+    double bottom = 4*pow(PI,2);
+    height = pow(top/bottom,(1/3));
+    height = height-RADIUSEARTH;
+    //Display result
+    printf("The height after %0.2lf seconds is: %0.2lf meters\n",period,height);
+    return 0;
+}
+// Function Definitions
+
+
