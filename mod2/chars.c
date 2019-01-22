2329967583edde3f1570d91a07e93121b9763e3f
Tyler Whitmore <tylerwhitmore@mail.weber.edu>
2019-01-22 09:50:33 -0700

    Correctly added chars back

--- mod2/chars.c
+++ mod2/chars.c
@@ -32,7 +32,7 @@ int main()
     printf("Enter your weight in kilograms: \n");
     scanf("%lf",&weight);
     printf("Enter your gender: \n");
-    scanf("%c",&sex);
+    scanf(" %c",&sex);
     printf("Your age is: %0.0lf\nYour weight is: %0.2lfkg\nYour gender is: %c\n",
             age,weight,sex);
     return 0;
