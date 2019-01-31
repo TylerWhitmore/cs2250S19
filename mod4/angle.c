6de705785b63b6094adbdd4e4b2b729413fecf1e
Tyler Whitmore <tylerwhitmore@mail.weber.edu>
2019-01-31 09:15:11 -0700

    Added Radians to angle.c

--- mod4/angle.c
+++ mod4/angle.c
@@ -23,14 +23,15 @@
 #include <math.h> //for fabs(decimals)
 #include <stdlib.h>//for abs
 // Constants
-
+#define PI 3.1415926
 // Function Prototypes
 
 // Main Function
 int main()
 {
     int input = 0;
-    printf("Please input an angle integer: ");
+    double radians = 0.0;
+    printf("Please input an angle integer in degrees: ");
     scanf("%d",&input);
     while(input < 0 || input > 360){
         if(input > -360 && input < 0){
@@ -43,20 +44,26 @@ int main()
             input = input - 360;
         }
     }
+    radians = input * (PI/180);
     if(input > 0 && input < 90){//quadrant1
-        printf("Your angle is in quadrant I\n");
+        printf("Your angle is: %d degrees and %.02lf radians.\n",input,radians);
+        printf("It is in quadrant I\n");
     }
     else if(input > 90 && input < 180){//quadrant2
-        printf("Your angle is in quadrant II\n");
+        printf("Your angle is: %d degrees and %.02lf radians.\n",input,radians);
+        printf("It is in quadrant II\n");
     }
     else if(input > 180 && input < 270){//quadrant3
-        printf("Your angle is in quadrant III\n");
+        printf("Your angle is: %d degrees and %.02lf radians.\n",input,radians);
+        printf("It is in quadrant III\n");
     }
     else if(input > 270 && input < 360){//quadrant4
-        printf("Your angle is in quadrant IV\n");
+        printf("Your angle is: %d degrees and %.02lf radians.\n",input,radians);
+        printf("It is in quadrant IV\n");
     }
     else{
-        printf("Your angle %d is not in a quadrant\n",input);
+        printf("Your angle is: %d degrees ad %.02lf radians.\n",input,radians);
+        printf("It is not in a quadrant\n");
     }
     return 0;
 }
