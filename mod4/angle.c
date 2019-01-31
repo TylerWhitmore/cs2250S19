5325661a5166e1086a52d83c6fe70a028c9a72e7
Tyler Whitmore <tylerwhitmore@mail.weber.edu>
2019-01-31 08:38:28 -0700

    Added angle.c

---
+++ mod4/angle.c
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  angle.c
+ *
+ *    Description: calculate the quadrant based on input angle
+ *                 Quadrant 1: angle between 0-90
+ *                 Quadrant 2: angle between 90-180
+ *                 Quadrant 3: angle between 180-270
+ *                 Quadrant 4: angle between 270-360
+ *
+ *        Version:  1.0
+ *        Created:  01/31/2019 08:35:06 AM
+ *       Revision:  none
+ *       Compiler:  gcc angle.c -o angle.out
+ *
+ *         Author:  Tyler Whitmore (), tylerwhitmore@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <math.h>
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    
+    return 0;
+}
+// Function Definitions
+
+
